ece86abb92372fa954d1e9f4e0363856b1ee2d78
NickYuzdepski <91340642+NickYuzdepski@users.noreply.github.com>
2021-11-24 12:11:37 -0600

    added button class, toggleState function and edited mousePressed function

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@
 // Nicklas Yuzdepski
 // November 18, 2021
 
+let button;
 let backgroundImage;
 
 function preload() {
@@ -10,6 +11,7 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  button = new Button();
 }
 
 function draw() {
@@ -17,5 +19,34 @@ function draw() {
 }
 
 function mousePressed() {
+  if (mouseX = this.x + this.width && (mouseY = this.y + this.length)) {
+    toggleState()
+  }
+}
+
+class Button {
+  constructor(x, y, height, width, color, text ) {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+    this.color = color;
+    this.text = text;
+  }
+
+display() {
+  fill(this.color);
+  rect(this.x, this.y, this.height, this.width);
+}
+
+toggleState() {
+  let state = 1;
+}
+
+
+
+
+
+
 
 }
